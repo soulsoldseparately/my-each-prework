44f5915e0197e67829b83f9841933b38798712b9
Daniel Toma <danieltoma@ubalumni.com>
2018-12-04 05:52:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(array)
   # code here
+  counter = 0
+  
+  while counter < array.length
+    yield
+    array do |counter|
+      puts array[counter]
+    end
+ 
+    counter = counter + 1
+  end
+  
 end
